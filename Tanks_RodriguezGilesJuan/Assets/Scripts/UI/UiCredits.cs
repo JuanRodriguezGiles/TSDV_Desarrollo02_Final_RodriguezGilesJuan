0976c90902f0c70adc67e59b2ff129ef20186cf7
Juan <juanrodriguezgiles@gmail.com>
2021-07-26 19:12:36 -0300

    Implemented basic main menu and credits scene

---
+++ Tanks_RodriguezGilesJuan/Assets/Scripts/UI/UiCredits.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UiCredits : MonoBehaviour
+{
+    public void LoadMainMenuScene()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Tanks_RodriguezGilesJuan/Assets/Scripts/UI/UiMainMenu.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+public class UiMainMenu : MonoBehaviour
+{
+    public void LoadGameplayScene()
+    {
+        SceneManager.LoadScene("Gameplay");
+    }
+    public void LoadCreditsScene()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
