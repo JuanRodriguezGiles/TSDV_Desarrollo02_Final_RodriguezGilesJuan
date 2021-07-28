31c06107d37011cea652a6490838c7446af8deb7
Juan <juanrodriguezgiles@gmail.com>
2021-07-28 13:17:02 -0300

    Updated player movement so it stays perpendicular to the terrain

---
+++ Tanks_RodriguezGilesJuan/Assets/Scripts/Player/PerpendicularMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+public class PerpendicularMovement : MonoBehaviour
+{
+    public float movementSpeed;
+    public float rotationSpeed;
+    public Transform raycastPoint;
+    public GameObject tankModel;
+
+    Vector3 _forwardDirection;
+    float _rotationInput;
+    float _movementInput;
+
+    void Update()
+    {
+        _movementInput = Input.GetAxis("Vertical");
+        _rotationInput = Input.GetAxis("Horizontal") * rotationSpeed;
+    }
+    void FixedUpdate()
+    {
+        GroundTank();
+        RotateTank();
+        MoveTank();
+    }
+    void GroundTank()
+    {
+        RaycastHit hit;
+        Physics.Raycast(raycastPoint.position, Vector3.down, out hit);
+        transform.up -= (transform.up - hit.normal) * 0.1f;
+    }
+    void RotateTank()
+    {
+        _rotationInput *= Time.deltaTime;
+        tankModel.transform.Rotate(0.0f, _rotationInput, 0.0f);
+    }
+    void MoveTank()
+    {
+        _forwardDirection = tankModel.transform.forward;
+        transform.position += _forwardDirection * Time.deltaTime * movementSpeed * _movementInput;
+    }
+}
