960e2064a0c4a8453a05efedeff1c84b29b14546
Juan <juanrodriguezgiles@gmail.com>
2021-07-29 18:20:25 -0300

    Implemented collision detection for projectile with crates

---
+++ Tanks_RodriguezGilesJuan/Assets/Scripts/Crate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+public class Crate : MonoBehaviour, IHittable
+{
+    public ParticleSystem explosion;
+    public void OnHit()
+    {
+        explosion.transform.parent = null;
+        explosion.Play();
+        Destroy(explosion.gameObject, explosion.main.duration);
+        Destroy(gameObject);
+    }
+}
---
+++ Tanks_RodriguezGilesJuan/Assets/Scripts/Projectile.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+public class Projectile : MonoBehaviour
+{
+    public float hitRadius;
+    int _layerMask;
+    void Awake()
+    {
+        _layerMask = 1 << 8;
+        _layerMask = ~_layerMask;
+    }
+    void OnTriggerEnter(Collider collider)
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, hitRadius, _layerMask);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            Rigidbody targetRigidbody = colliders[i].GetComponent<Rigidbody>();
+            if (!targetRigidbody)
+                continue;
+            targetRigidbody.GetComponent<IHittable>().OnHit();
+            Destroy(gameObject);
+        }
+    }
+}
