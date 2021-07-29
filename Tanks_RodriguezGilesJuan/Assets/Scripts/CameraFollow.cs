c30338a57d8ba91de37fa0c4f032616061a738f3
Juan <juanrodriguezgiles@gmail.com>
2021-07-29 18:25:29 -0300

    Implemented camera follow script

---
+++ Tanks_RodriguezGilesJuan/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Vector3 _offset;
+    [SerializeField] private Vector3 _rotation;
+    public Transform _playerTransform;
+    void LateUpdate()
+    {
+        Vector3 targetPosition = _playerTransform.position + _offset;
+        transform.position = Vector3.Slerp(transform.position, targetPosition, 0.5f);
+        transform.rotation = Quaternion.Euler(_rotation);
+    }
+}
