1c8ce3f887a7f7656ad6cfecc3afed8538c123c7
Juan <juanrodriguezgiles@gmail.com>
2021-07-31 16:31:24 -0300

    Created GameManager for timer management

---
+++ Tanks_RodriguezGilesJuan/Assets/Scripts/GameManager.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] float _maxTimeSeconds;
+    public static event Action<float> onTimeChange;
+    float _time;
+    void Start()
+    {
+        _time = _maxTimeSeconds;
+        StartCoroutine(Timer());
+    }
+    IEnumerator Timer()
+    {
+        while (_time > 0)
+        {
+            _time -= Time.deltaTime;
+            onTimeChange?.Invoke(_time);
+            yield return null;
+        }
+        GameOver();
+    }
+    void GameOver()
+    {
+
+    }
+}
---
+++ Tanks_RodriguezGilesJuan/Assets/Scripts/UI/UiGameplay.cs
@@ -0,0 +1,31 @@
+﻿using TMPro;
+using UnityEngine;
+public class UiGameplay : MonoBehaviour
+{
+    public TMP_Text scoreText;
+    public TMP_Text timeText;
+    void OnEnable()
+    {
+        GameManager.onTimeChange += UpdateTimer;
+    }
+    void OnDisable()
+    {
+        GameManager.onTimeChange -= UpdateTimer;
+    }
+    void UpdateTimer(float time)
+    {
+        timeText.text = Mathf.RoundToInt(time).ToString();
+    }
+    void UpdateScore(int score)
+    {
+        scoreText.text = score.ToString();
+    }
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+    }
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+    }
+}
